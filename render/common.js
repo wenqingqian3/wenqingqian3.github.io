bab89f11930f76f7f109c464272e7dba243adb86
wenqingqian <1576395103@qq.com>
2024-10-03 22:19:33 +0800

    style of render

--- render/common.js
+++ render/common.js
@@ -28,12 +28,12 @@ function toggleDarkMode() {
 }
 
 window.onload = function() {
-    const root = document.documentElement;
-    const savedMaxWidth = localStorage.getItem('max-width-show');
+	const root = document.documentElement;
+	const savedMaxWidth = localStorage.getItem('max-width-show');
 
-    if (savedMaxWidth) {
-        root.style.setProperty('--max-width-show', savedMaxWidth);
-    }
+	if (savedMaxWidth) {
+		root.style.setProperty('--max-width-show', savedMaxWidth);
+	}
 };
 
 // 调整 max-width 的函数
@@ -49,21 +49,21 @@ function adjustMaxWidthAdd() {
 	} else if (currentMaxWidth === '600px') {
 		newMaxWidth = '700px';
 	} else if (currentMaxWidth === '700px') {
-        newMaxWidth = '800px';
+		newMaxWidth = '800px';
 	} else if (currentMaxWidth === '800px') {
-        newMaxWidth = '900px'
+		newMaxWidth = '900px'
 	} else if (currentMaxWidth === '900px') {
-        newMaxWidth = '1000px'
+		newMaxWidth = '1000px'
 	} else if (currentMaxWidth === '1000px') {
-        newMaxWidth = '1100px'
+		newMaxWidth = '1100px'
 	} else if (currentMaxWidth === '1100px') {
-        newMaxWidth = '1200px'
+		newMaxWidth = '1200px'
 	} else {
 		newMaxWidth = '1200px'
 	}
 
-    root.style.setProperty('--max-width-show', newMaxWidth);
-    localStorage.setItem('max-width-show', newMaxWidth);
+	root.style.setProperty('--max-width-show', newMaxWidth);
+	localStorage.setItem('max-width-show', newMaxWidth);
 }
 
 function adjustMaxWidthSub() {
@@ -86,13 +86,13 @@ function adjustMaxWidthSub() {
 	} else if (currentMaxWidth === '600px') {
 		newMaxWidth = '500px';
 	} else if (currentMaxWidth === '500px') {
-        newMaxWidth = '400px';
+		newMaxWidth = '400px';
 	} else {
 		newMaxWidth = '400px';
 	}
 
-    root.style.setProperty('--max-width-show', newMaxWidth);
-    localStorage.setItem('max-width-show', newMaxWidth);
+	root.style.setProperty('--max-width-show', newMaxWidth);
+	localStorage.setItem('max-width-show', newMaxWidth);
 }
 
 
